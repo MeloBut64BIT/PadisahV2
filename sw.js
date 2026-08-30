c82dd4b47432d1c4d2fdc6b0c5108dc2616e4cb1
Melihhan YAVUZ <melihhany@gmail.com>
2026-08-30 23:11:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = "padisah-v1";
+const CACHE_NAME = "padisah-v2"; // Updated version to break old cache
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,21 +6,48 @@ const ASSETS = [
   "./game.js",
   "./manifest.json",
   "./data/events.js",
-  "./assets/sahne.gif",
+  "./assets/bg.jpg",
+  "./assets/padisah.png",
+  "./assets/vezir.png",
+  "./assets/serdar.png",
+  "./assets/hazine.png",
   "./assets/icon.png"
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting(); // Force the new service worker to take over immediately
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => console.log('Cache error', err));
     })
   );
 });
 
+// Clear old caches when a new version is activated
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Take control of all clients immediately
+  );
+});
+
+// Network First Strategy: Always try to get the newest files from GitHub.
+// If offline (no internet), fall back to the cached files.
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+  );
+});
